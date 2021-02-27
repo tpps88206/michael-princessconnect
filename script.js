d8a6ed550835da67063f119cf14565b299928e88
Sheng Chuan (Michael) Hsu <cool96482@hotmail.com>
2021-02-27 13:38:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,55 @@
-function myFunction() {
+function calculate() {
   var content = document.getElementById("content").value;
   var offset = document.getElementById("offset").value;
   var result = "";
   var sec = (90 - offset) * 1000;
   
-  document.getElementById("output").value = "轉換中...";
+  document.getElementById("output").value = "转换中...";
   
   for (var line of content.split("\n")) {
     var time = line.substr(0, 4);
     
     time =  time.replace(".", ":");
-    time =  time.replace("''", ":");
+    time =  time.replace("'", ":");
+    
+    // If the time does not add a colon, it will be processed separately
+    if (time.indexOf(":") < 0) {
+    	time = noSymbolTransform(time);
+    }
     
     var oldDate = new Date("2020-01-01T10:0" + time);
     
-    if (!!oldDate) {
+        
+    if (!isNaN(oldDate)) {
       var newDate = new Date(oldDate - sec);
     
       if (newDate.getTime() > new Date("2020-01-01T09:59:59").getTime()) {
         var zero = "";
-        // 如果秒數小於 10 秒，則在前面補 0
+        // If the number of seconds is less than 10 seconds, add 0 in front
         if (newDate.getSeconds() < 10) {
           zero = "0";
         }
         result = result + newDate.getMinutes() + ":" + zero + newDate.getSeconds() + line.substr(4) + "\n";
       }
     } else {
+    	// Text line without time
       result = result + line + "\n";
     }
   }
   
   document.getElementById("output").value = result;
+}
+
+function noSymbolTransform(time) {
+	var i = parseInt(time);
+  
+  if (i > 99) {
+  	i = i - 100;
+    if (i > 9) {
+    	return "1:" + i;
+    } 
+    return "1:0" + i;
+  } else {
+  	return "0:" + i;
+  }
 }
